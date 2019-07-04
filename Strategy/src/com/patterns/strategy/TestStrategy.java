a709580e696d95ccb3f4d7f0c0a0b65d6f795c1b
Alfie <alfie.developer@gmail.com>
2019-07-04 18:03:09 +0300

    [PatternsCollection] Small issues fix
    Strategy pattern example is successfully applied

--- Strategy/src/com/patterns/strategy/TestStrategy.java
+++ Strategy/src/com/patterns/strategy/TestStrategy.java
@@ -1,19 +1,26 @@
 package com.patterns.strategy;
 
-import com.patterns.strategy.flying.duck.*;
-import com.patterns.strategy.flying.*;
-import com.patterns.strategy.quack.*;
+import com.patterns.strategy.duck.DecoyDuck;
+import com.patterns.strategy.duck.Duck;
+import com.patterns.strategy.duck.MallardDuck;
+import com.patterns.strategy.flying.FlyNoWay;
+import com.patterns.strategy.flying.FlyRocketPowered;
+import com.patterns.strategy.quack.Squeak;
 
 public class TestStrategy {
 
     public static void main(String [] args) {
-        Duck mullard = new MullardDuck();
+        Duck mullard = new MallardDuck();
         mullard.performQuack();
         mullard.performFly();
 
-        Duck model = new ModelDuck();
+        Duck model = new DecoyDuck();
         model.performQuack();
-        model.setFlyBehaviour(new FlyRocketPower());
+        model.setQuackBehaviour(new Squeak());
+        model.performQuack();
+        model.setFlyBehaviour(new FlyRocketPowered());
+        model.performFly();
+        model.setFlyBehaviour(new FlyNoWay());
         model.performFly();
     }
 }
